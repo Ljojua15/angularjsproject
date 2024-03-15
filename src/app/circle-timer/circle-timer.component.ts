0d61549823517b1c0e62522e084930fcc4de1231
Ljojua15 <jojualasha15@gmail.com>
2024-03-15 15:58:29 +0400

    hhhh

--- src/app/circle-timer/circle-timer.component.ts
+++ src/app/circle-timer/circle-timer.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, SimpleChanges} from '@angular/core';
+import {Component, ElementRef, Input, SimpleChanges, ViewChild, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-circle-timer',
@@ -8,10 +8,17 @@ import {Component, Input, SimpleChanges} from '@angular/core';
 export class CircleTimerComponent {
   // @ts-ignore
   @Input() progress: number = 1000;
+  @ViewChild('swipercolor') swipercolor!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+  ngAfterViewInit() {
+
+  }
 
   ngOnInit(): void {
     let progress =
     this.loadData();
+
   }
 
   loadData() {
@@ -24,6 +31,29 @@ export class CircleTimerComponent {
 
   ngOnChanges(changes: SimpleChanges): void {
     this.loadData();
+    this.moveSwiperColor(0); // Initialize swiper-color position
+  }
+  moveSwiperColor(position: number) {
+    const swiperColor = this.swipercolor.nativeElement;
+    this.renderer.setStyle(swiperColor, 'transition', 'left 0.5s ease-out');
+    switch (position) {
+      case 1:
+        this.renderer.setStyle(swiperColor, 'left', '5px');
+        this.renderer.setStyle(swiperColor, 'right', 'auto');
+        break;
+      case 2:
+        this.renderer.setStyle(swiperColor, 'left', '33.5%');
+        break;
+      case 3:
+        this.renderer.setStyle(swiperColor, 'left', 'auto');
+        this.renderer.setStyle(swiperColor, 'left', '65.5%');
+        break;
+      default:
+        this.renderer.setStyle(swiperColor, 'left', '33.5%');
+        this.renderer.setStyle(swiperColor, 'right', 'auto');
+        this.renderer.setStyle(swiperColor, 'display', 'block');
+        break;
+    }
   }
   }
 
