8f721bbd6d12977013988be866e26566d6ccc0c7
Ljojua15 <jojualasha15@gmail.com>
2024-03-17 12:08:52 +0400

    changetime

--- src/app/circle-timer/circle-timer.component.ts
+++ src/app/circle-timer/circle-timer.component.ts
@@ -10,36 +10,64 @@ export class CircleTimerComponent {
   @Input() progress: number = 1000;
   @ViewChild('swipercolor') swipercolor!: ElementRef;
 
+
+  firstTimer:number =10
+  secondTimer:number =20
+  thirdTimer:number =30
+
+  mainTimer : number = this.firstTimer
+
   constructor(private renderer: Renderer2) {}
   ngAfterViewInit() {
 
+    
+    
   }
+
+
 
   ngOnInit(): void {
-    let progress = 
+ 
     this.loadData();
 
+    
   }
+
 
   loadData() {
     let scrollProgress = document.getElementById('progress');
 
     // @ts-ignore
     scrollProgress.style.background = `conic-gradient(#161932 ${this.progress}%, #f87070 0%)`;
+    this.mainTimer = this.secondTimer 
+
   }
   loadData1() {
     let scrollProgress = document.getElementById('progress');
 
     // @ts-ignore
     scrollProgress.style.background = `conic-gradient(#161932 ${this.progress}%, #8ba320 0%)`;
+    this.mainTimer = this.firstTimer
   }
+  loadData2() {
+    let scrollProgress = document.getElementById('progress');
+
+    // @ts-ignore
+    scrollProgress.style.background = `conic-gradient(#161932 ${this.progress}%, #8ba320 0%)`;
+    this.mainTimer = this.thirdTimer
+  }
+
+
 
 
 
   ngOnChanges(changes: SimpleChanges): void {
     this.loadData();
-    this.moveSwiperColor(0); // Initialize swiper-color position
+    this.moveSwiperColor(0);
   }
+
+
+
   moveSwiperColor(position: number) {
     
     const swiperColor = this.swipercolor.nativeElement;
@@ -48,27 +76,38 @@ export class CircleTimerComponent {
       case 1:
         this.renderer.setStyle(swiperColor, 'left', '5px');
         this.renderer.setStyle(swiperColor, 'right', 'auto');
+        this.loadData()
+        console.log(this.firstTimer)
+    
 
         break;
       case 2:
         this.renderer.setStyle(swiperColor, 'left', '33.5%');
-  
+        this.loadData1()
+ 
         break;
       case 3:
         this.renderer.setStyle(swiperColor, 'left', 'auto');
         this.renderer.setStyle(swiperColor, 'left', '65.5%');
-
+        this.loadData2()
+        console.log(this.thirdTimer);
+  
+        
         break;
       default:
         this.renderer.setStyle(swiperColor, 'left', '33.5%');
         this.renderer.setStyle(swiperColor, 'right', 'auto');
         this.renderer.setStyle(swiperColor, 'display', 'block');
         this.loadData1()
+        console.log(this.firstTimer)
         break;
 
     }
 
 
   }
+
+
+
   }
 
