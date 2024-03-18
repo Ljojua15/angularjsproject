afd27cecbc0176bf6362c382b923cb041cdb86f3
Ljojua15 <jojualasha15@gmail.com>
2024-03-18 15:34:17 +0400

    hsghjg

--- src/app/circle-timer/circle-timer.component.ts
+++ src/app/circle-timer/circle-timer.component.ts
@@ -9,13 +9,15 @@ export class CircleTimerComponent {
   // @ts-ignore
   @Input() progress: number = 1000;
   @ViewChild('swipercolor') swipercolor!: ElementRef;
+  @ViewChild('colorCircle') colorCircle!: ElementRef;
 
 
-  firstTimer:number =50
+  firstTimer:number =20
   secondTimer:number =10
   thirdTimer:number =30
 
   mainTimer !: number
+  colorChange : string = '#f87070'
 
   constructor(private renderer: Renderer2) {}
   ngAfterViewInit() {
@@ -31,20 +33,26 @@ export class CircleTimerComponent {
 
 
   loadData(arg:number, type:string) {
-    let scrollProgress = document.getElementById('progress');
-    let color = 'red'
+    const colorCircle = this.colorCircle.nativeElement
+    this.renderer.setStyle(colorCircle, 'background-color', 'blue');
+
+
+
     if(type === 'first') {
-       color = 'blue'
+      this.colorChange ='blue';
+      this.renderer.setStyle(colorCircle, 'background-color', 'blue');
     }else if(type === 'second') {
-      color = '#f87070'
+      this.colorChange ='#f87070';
+      this.renderer.setStyle(colorCircle, 'background-color', '#f87070');
     }else if(type === 'third') {
-      color = 'red'
+      this.colorChange ='red';
+      this.renderer.setStyle(colorCircle, 'background-color', 'red');
 
     }
-    console.log(color)
+
 
     // @ts-ignore
-    scrollProgress.style.background = `conic-gradient( #161932 ${this.progress}%, ${color} 0%)`;
+    this.renderer.setStyle(colorCircle, 'background', `conic-gradient( #161932 ${this.progress}%, ${this.colorChange} 0%)`);
     this.mainTimer = arg
   }
 
@@ -72,8 +80,6 @@ export class CircleTimerComponent {
         this.renderer.setStyle(swiperColor, 'right', 'auto');
         this.loadData(this.firstTimer, 'first')
         console.log(this.firstTimer)
-
-
         break;
       case 2:
         this.renderer.setStyle(swiperColor, 'left', '33.5%');
