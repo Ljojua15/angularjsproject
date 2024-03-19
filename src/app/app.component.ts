98cc87c65c369f4785b82fbe372974d02dc789e8
Ljojua15 <jojualasha15@gmail.com>
2024-03-19 18:47:42 +0400

    hsghjsadadsfsdfsdg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { take } from 'rxjs/operators';
 })
 export class AppComponent implements  OnInit, OnDestroy {
   progress: number = 0;
-  noOfFiles: number = 0;
+  noOfFiles: number = 1;
   completed: boolean = false;
   changeTime: number = this.noOfFiles * 60;
   subscription: Subscription | undefined;
@@ -75,6 +75,7 @@ export class AppComponent implements  OnInit, OnDestroy {
       setTimeout(() =>{
         this.stop()
       }, 5000)
+
     }
   }
 
